9f6a6f17dcbe272cec96630b6ff0bc43c02d7e28
Victor Carreras <ctt.victorcarreras@gmail.com>
2023-05-12 16:13:52 -0300

    iniciando BlogPessoal com React

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img className="img" src="https://uploads.metropoles.com/wp-content/uploads/2019/05/02185226/bolsonaro-anime.jpg" alt="Imagem tela inicial" />
+        </>
+    );
+}
+
+export default Home;
